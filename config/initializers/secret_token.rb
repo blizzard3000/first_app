9addcd71f18e98bc1f99dd1d5bb8d73acafe293c
blizzard3000 <blizzard3000@me.com>
2013-06-11 12:41:55 +0200

    ignored log files and tmp dir

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9b6326dba3d659485f2adb93cf3ae9c86ea5c8ab2e09063e3e24a05f2d978843996f0b41306110a50a18e89e1e50ac6b3ba945c1801fc5708e90dc1602564603'
